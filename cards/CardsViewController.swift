8e98d4ae673e6b43a138ee6377e9107967919993
ToshiroSama <alishdjuraev@gmail.com>
2023-01-30 09:33:41 +0500

    Make a auto-layout constraints programmatically

---
+++ cards/CardsViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import SnapKit
+
+class CardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+    
+}
+
